018b5aad1bbce29998acbd928f16d101f3dd0abd
Dennis <ngetichdennis90@gmail.com>
2023-12-20 22:10:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 
 // Write your code here
+// Set variables num1 and num2
+let num1 = 31;
+let num2 = 2;
+
+// Multiply num1 and num2 using an equation
+let multiply = num1 * num2;
+
+// Log the result to the console
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 100) + 1;
+console.log (random);
+
+// Declare variables num3 and num4
+let num3 = 16;
+let num4 = 12;
+
+// Calculate the remainder of dividing num3 by num4
+let mod = num3 % num4;
+
+// Log the result to the console
+console.log(mod);
+
+
+// Declare variables
+let number1 = 5;
+let number2 = 10;
+let number3 = 20;
+
+// Calculate the maximum number in the set
+let max = Math.max(number1, number2, number3);
+
+// Log the result to the console
+console.log(max);
